7c4cfd69267232e9ea7180f7ff2e66894b1bebaf
muester <169181335+muester@users.noreply.github.com>
2025-12-09 23:08:57 +0100

    Day 9 solution

--- src/main/kotlin/d9.kt
+++ src/main/kotlin/d9.kt
@@ -1,34 +1,66 @@
 import java.io.File
 
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
-fun area(a: Pair<Int,Int>, b: Pair<Int,Int>): Long {
-    val width = abs(a.first-b.first)+1
-    val height = abs(a.second-b.second)+1
-    return (width.toLong()*height.toLong())
+class Rect(val xLow: Int, val xHigh: Int, val yLow: Int, val yHigh: Int, val area: Long)
+
+fun sortedRectangle(a: Pair<Int,Int>, b: Pair<Int,Int>): Rect {
+    val xLow = min(a.first, b.first)
+    val xHigh = max(a.first, b.first)
+    val yLow = min(a.second, b.second)
+    val yHigh = max(a.second, b.second)
+    return Rect(xLow,xHigh,yLow,yHigh,(abs(xHigh-xLow)+1).toLong() * (abs(yHigh-yLow)+1).toLong())
 }
-
 
 fun main() {
 
     val file = File("src/main/resources/d9.txt")
 
     val points = mutableListOf<Pair<Int,Int>>()
+    val rectangles = mutableListOf<Rect>()
 
-    file.forEachLine {
-        val (a,b) = it.split(",").map { it.toInt() }
-        points += a to b
+    var partOne: Long = 0
+    var partTwo: Long = 0
+
+    file.forEachLine { line ->
+        points += line.split(",").map { it.toInt() }.let{ Pair(it[0], it[1]) }
     }
 
-    var topArea: Long = 0
-
-    for(i in points.indices){
-        for(j in points.indices){
-            if(i == j) continue
-            val newArea = area(points[i], points[j])
-            topArea = if(topArea > newArea) topArea else newArea
+    for (i in points.indices) {
+        for (j in i + 1 until points.size) {
+            rectangles += sortedRectangle(points[i], points[j])
         }
     }
 
-    println("Part 1: $topArea")
+    partOne = rectangles.maxByOrNull { it.area }?.area ?: 0
+
+    var largest: Long = 0
+
+    for (r in rectangles) {
+
+        var clipped = false
+
+        for(i in points.indices) {
+            val borderLine = sortedRectangle(points[i], points.getOrElse(i+1) {points[0]})
+            // Check for intersects with any edge
+            if(borderLine.xLow + 1 > r.xHigh || borderLine.xHigh - 1 < r.xLow||
+                borderLine.yLow + 1 > r.yHigh || borderLine.yHigh - 1 < r.yLow) {
+                continue
+            }
+            clipped = true
+            break
+        }
+
+        if (!clipped) {
+            if (r.area > largest) {
+                largest = r.area
+            }
+        }
+    }
+
+    partTwo = largest
+
+    println("Part 1: $partOne, Part 2: $partTwo")
 }
